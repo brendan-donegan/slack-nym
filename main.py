f5117fda513c96936d84288e7a26cb26ca07070f
Brendan Donegan <brendan.j.donegan@gmail.com>
2016-12-25 22:06:19 +0530

    Expand acronyms now

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from flask import (
 
 app = Flask(__name__)
 
+def get_acronyms():
+    with open('acronyms.txt') as acronyms:
+        return {a.split('=')[0].strip(): a.split('=')[1].strip()
+                for a in acronyms.readlines()}
+
 @app.route('/')
 def respond():
     with open('slack_token') as slack_token:
         token = slack_token.read().strip()
     if request.args['token'] == token:
-        return request.args['text']
+        return get_acronyms().get([request.args['text'], request.args['text'])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
