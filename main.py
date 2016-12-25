12b32d3c56edef3c27169ea37097bfba43115cb1
Brendan Donegan <brendan.j.donegan@gmail.com>
2016-12-25 22:19:00 +0530

    Expand for each work in text

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ from flask import (
 
 app = Flask(__name__)
 
+acronyms = get_acronyms()
+
+def expand_acronym(word):
+    return acronyms.get(word, word)
+
 def get_acronyms():
     with open('acronyms.txt') as acronyms:
         return {a.split('=')[0].strip(): a.split('=')[1].strip()
@@ -16,7 +21,7 @@ def respond():
     with open('slack_token') as slack_token:
         token = slack_token.read().strip()
     if request.args['token'] == token:
-        return get_acronyms().get(request.args['text'], request.args['text'])
+        return ' '.join(expand_acronym(word) for word in request.args['text'])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
